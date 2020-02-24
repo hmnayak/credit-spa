335b6d282c46568a6758170aa077306f5a8995f6
hmnayak <hmaheshnayak@outlook.com>
2020-02-24 15:31:30 +0530

    Modified Payment model to accept amount as numeric type

--- model/transaction.go
+++ model/transaction.go
@@ -14,7 +14,7 @@ type Payment struct {
 	ID         int     `db:"id" json:"id,string"`
 	CustomerID int     `db:"customer_id" json:"customerid"`
 	Date       string  `db:"date" json:"date"`
-	Amount     float64 `db:"amount" json:"amount,string"`
+	Amount     float64 `db:"amount" json:"amount"`
 	Mode       *string `db:"mode" json:"mode"`
 	Remarks    *string `db:"remarks" json:"remarks"`
 }
