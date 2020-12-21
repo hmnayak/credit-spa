bfc4a087cac3a0bebf5a099054579d9ce76f3c77
usrlocl <usrlocl@gmail.com>
2020-12-21 22:02:51 +0530

    upgrade to latest packages - react, framework7, babel

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -3,24 +3,26 @@ import { App, View, Navbar } from 'framework7-react';
 import HomePage from './pages/home.jsx';
 import AboutPage from './pages/about.jsx';
 
-const params = {
-  name: 'Credit',
-  theme: 'auto',
-  id: 'treeples.credit',
-  routes: [{
-    path: '/',
-    component: HomePage,
-  },{
-    path: '/about',
-    component: AboutPage,
-  }],  
-};
+const routes = [{
+  path: '/',
+  component: HomePage,
+}, {
+  path: '/about',
+  component: AboutPage,
+}];
 
 const rootPath = window.location.pathname.replace(/\/+$/, '');
 
 export default () => (
-  <App params={params}>
+  <App name="Credit" theme="auto" id="treeples.credit" routes={routes}>
     <Navbar title="Credit"></Navbar>
-    <View main pushState={true} pushStateSeparator="" pushStateRoot={rootPath} />
+    <View
+      main
+      url="/"
+      browserHistory
+      browserHistorySeparator=""
+      browserHistoryRoot={rootPath}
+      animate={false}
+    />
   </App>
 );
