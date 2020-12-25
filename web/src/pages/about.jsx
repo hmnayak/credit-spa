06a4383dc8d3539ff4db721173d278cb5a7e5017
MIV_MMXX <rakeshkaranth@gmail.com>
2020-12-25 14:10:51 +0100

    Seperated Ajax calls from UI

--- web/src/pages/about.jsx
+++ web/src/pages/about.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Page, Link } from "framework7-react";
-import Infotext from "../api";
+import { aboutinfoApi } from "../api";
 
 export default () => {
   return (
@@ -11,3 +11,41 @@ export default () => {
     </Page>
   );
 };
+
+function Infotext() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [item, setItem] = useState("");
+
+  let aboutPromise = aboutinfoApi();
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    aboutPromise
+      .then((res) => res.text())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          console.log(result);
+          setItem(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return <div> {item}</div>;
+  }
+}
