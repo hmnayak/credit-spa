5067e79c08e78289168b928c8529e3eb306f11ae
MIV_MMXX <rakeshkaranth@gmail.com>
2020-12-26 16:55:38 +0100

    Updated Loading for while AboutInfo loaded

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { App, View, Navbar } from "framework7-react";
 import HomePage from "./pages/home.jsx";
 import AboutPage from "./pages/about.jsx";
 
-const routes = [
-  {
-    path: "/",
-    component: HomePage,
-  },
-  {
-    path: "/about",
-    component: AboutPage,
-  },
-];
-
-export function Loading() {
-  return <div>Loading...</div>;
-}
-
 const rootPath = window.location.pathname.replace(/\/+$/, "");
 
-export default () => (
-  <App name="Credit" theme="auto" id="treeples.credit" routes={routes}>
-    <Navbar title="Credit"></Navbar>
-    <View
-      main
-      url={rootPath}
-      browserHistory
-      browserHistorySeparator=""
-      browserHistoryRoot=""
-      animate={false}
-    />
-  </App>
-);
+export default () => {
+  const [isLoading, setLoading] = useState(false);
+
+  const Loading = () => {
+    if (isLoading) {
+      return <div>Loading...</div>;
+    }
+  };
+
+  const routes = [
+    {
+      path: "/",
+      component: HomePage,
+    },
+    {
+      path: "/about",
+      component: AboutPage,
+      options: {
+        props: {
+          loadComplete: setLoading,
+        },
+      },
+      beforeEnter: function (router) {
+        setLoading(true);
+        router.resolve();
+      },
+    },
+  ];
+
+  return (
+    <App name="Credit" theme="auto" id="treeples.credit" routes={routes}>
+      <Navbar title="Credit">{Loading()}</Navbar>
+      <View
+        main
+        url={rootPath}
+        browserHistory
+        browserHistorySeparator=""
+        browserHistoryRoot=""
+        animate={false}
+      />
+    </App>
+  );
+};
--- web/src/pages/about.jsx
+++ web/src/pages/about.jsx
@@ -1,52 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Page, Link } from "framework7-react";
 import { aboutinfoApi } from "../api";
-import { Loading } from "../app.jsx";
 
-export default () => {
-  return (
-    <Page>
-      <p>About</p>
-      <Infotext />
-      <Link href="/">Home</Link>
-    </Page>
-  );
-};
-
-const Infotext = (props) => {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+export default (props) => {
+  console.log(props);
   const [item, setItem] = useState("");
 
   let aboutPromise = aboutinfoApi();
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
   useEffect(() => {
     aboutPromise
       .then((res) => res.text())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          console.log(result);
-          setItem(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+      .then((result) => {
+        props.loadComplete(false);
+        setItem(result);
+      });
   }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return Loading();
-  } else {
-    return <div> {item}</div>;
-  }
+  return (
+    <Page>
+      <p>About</p>
+      <div> {item}</div>
+      <Link href="/">Home</Link>
+    </Page>
+  );
 };
