4139ad626a67ecdf2cc43320cc0c869f9afb2bf4
Mahesh Nayak <hmaheshnayak@gmail.com>
2021-03-06 18:28:11 +0100

    fix gstin data type and cleanup

--- model/db.go
+++ model/db.go
@@ -2,21 +2,6 @@ package model
 
 // Db specifies methods to manage resources
 type Db interface {
-	Login(string, string) (string, error)
-	CreateUserSession(string, string, string) error
-	GetUserSession(string) (AuthToken, error)
-	DeleteUserSession(string) error
-	ValidateUser(string) (AuthToken, error)
-	GetRoutes() ([]string, error)
-
-	UpdateCredit(Credit) error
-	DeleteCredit(int) error
-	CreatePayment(Payment) error
-	UpdatePayment(Payment) error
-	DeletePayment(int) error
-	GetCreditsByCustomer(int64) ([]*Credit, error)
-	GetPaymentsByCustomer(int64) ([]*Payment, error)
-
 	DoesUserExist(string) (bool, error)
 	CreateUser(string, string) (string, error)
 
